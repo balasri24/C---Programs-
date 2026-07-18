a9f11c26172ed6ef8feb71a16163213843951172
Balasri Kalavalapalli <balasrikalavalapalli24@gmail.com>
2026-07-18 07:47:48 +0530

    Add files via upload

---
+++ arthematical.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a=2,b=4;
+    printf("add a and b = %d \n",a+b );
+    printf("sub  a from b=%d \n",b-a);
+    printf("mult a and b =%d \n",a*b);
+    return 0;
+}
